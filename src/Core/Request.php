32c673c7d418a5f85454f5617b257aa4f288e21c
joakimunge <ashba@unge.se>
2017-11-03 15:27:49 +0100

    Add getters for path and method in Request.php

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -6,13 +6,21 @@ class Request {
     
     protected $path;
     protected $requestMethod;
-    protected $queryStrings = [];
-    protected $postValues = [];
+/*     protected $queryStrings = [];
+    protected $postValues = []; */
 
     public function __construct() {
         $parsedUrl = parse_url($_SERVER['REQUEST_URI']);
         $this->path = $parsedUrl['path'];
 
+    }
+    
+    public getPath() {
+        return $this->path;
+    }
+
+    public getRequestMethod() {
+        return $this->requestMethod;
     }
 }
 
