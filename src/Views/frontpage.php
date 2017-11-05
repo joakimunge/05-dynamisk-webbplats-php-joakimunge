47632fcf929673b9c88defa5f2e0b5c3182ce4d0
joakimunge <ashba@unge.se>
2017-11-05 21:02:37 +0100

    General styling of blog posts. Add asset folder

--- src/Views/frontpage.php
+++ src/Views/frontpage.php
@@ -1,2 +1,70 @@
-<main class="content">
+<main class="container">
+    <section class="content">
+        <section class="row">
+            <div class="col col-12 tags">
+                <ul class="tags__list">
+                    <li><a href="#" class="tags__btn">Web Development</a></li>
+                    <li><a href="#" class="tags__btn">CSS</a></li>
+                    <li><a href="#" class="tags__btn">PHP</a></li>
+                    <li><a href="#" class="tags__btn">Javascript</a></li>
+                    <li><a href="#" class="tags__btn">React</a></li>
+                    <li><a href="#" class="tags__btn">Angular</a></li>
+                    <li><a href="#" class="tags__btn">Frameworks</a></li>
+                    <li><a href="#" class="tags__btn">HTML</a></li>
+                </ul>
+            </div>
+        </section>
+        <section class="row gutters blog">
+            <article class="col col-3 blog__post">
+                <div class="blog__post-image"><img src="./assets/img/blog-test.jpg" alt="" width=100%></div>
+                <div class="blog__post-content">
+                <h4 class="blog__post-title">This is a blog post</h4>
+                <h5 class="blog__post-author">by Joakim Unge</h5>
+                <p class="blog__post-timestamp">November 6, 2017</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In at mattis dolor. 
+                Etiam dapibus ultrices volutpat. 
+                Donec aliquam ipsum nibh, vel mattis lectus efficitur et. Curabitur consectetur diam non auctor eleifend. </p>
+                </div>
+            </article>
+            <article class="col col-3 blog__post">
+                <div class="blog__post-image"><img src="./assets/img/blog-test.jpg" alt="" width=100%></div>
+                <div class="blog__post-content">
+                <h4 class="blog__post-title">This is a blog post</h4>
+                <h5 class="blog__post-author">by Joakim Unge</h5>
+                <p class="blog__post-timestamp">November 6, 2017</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In at mattis dolor. 
+                Etiam dapibus ultrices volutpat. 
+                Donec aliquam ipsum nibh, vel mattis lectus efficitur et. Curabitur consectetur diam non auctor eleifend. </p>
+                </div>
+            </article>
+            <article class="col col-3 blog__post">
+                <div class="blog__post-image"><img src="./assets/img/blog-test.jpg" alt="" width=100%></div>
+                <div class="blog__post-content">
+                <h4 class="blog__post-title">This is a blog post</h4>
+                <h5 class="blog__post-author">by Joakim Unge</h5>
+                <p class="blog__post-timestamp">November 6, 2017</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In at mattis dolor. 
+                Etiam dapibus ultrices volutpat. 
+                Donec aliquam ipsum nibh, vel mattis lectus efficitur et. Curabitur consectetur diam non auctor eleifend. </p>
+                </div>
+            </article>
+            <article class="col col-3 blog__post">
+                <div class="blog__post-image"><img src="./assets/img/blog-test.jpg" alt="" width=100%></div>
+                <div class="blog__post-content">
+                <h4 class="blog__post-title">This is a blog post</h4>
+                <h5 class="blog__post-author">by Joakim Unge</h5>
+                <p class="blog__post-timestamp">November 6, 2017</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In at mattis dolor. 
+                Etiam dapibus ultrices volutpat. 
+                Donec aliquam ipsum nibh, vel mattis lectus efficitur et. Curabitur consectetur diam non auctor eleifend. </p>
+                </div>
+            </article>
+        </section>
+    </section>
+
+
 </main>
