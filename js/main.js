3e10ba02473bfbea74557cf6c547521e5dd4fd33
joakimunge <ashba@unge.se>
2017-11-13 12:31:03 +0100

    Prevent default action on tags

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const tags = document.querySelectorAll('.tags__btn');
 
 for(let i = 0; i < tags.length; i++) {
-    tags[i].addEventListener('click', function() {
+    tags[i].addEventListener('click', function(e) {
+        e.preventDefault();
         tags[i].classList.toggle('tags__btn--active');
     });
 }
