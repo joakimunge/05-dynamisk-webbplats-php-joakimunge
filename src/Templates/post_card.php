33ad892d190431828469cc678cb421cc567ace9b
Joakim Unge <ashba@unge.se>
2017-11-16 18:56:54 +0100

    Add edit and favorite icon to blog-post cards

--- src/Templates/post_card.php
+++ src/Templates/post_card.php
@@ -7,5 +7,7 @@
         <h5 class="blog__post-author">by <?php echo $post->getAuthor(); ?></h5>
         <p class="blog__post-timestamp"><?php echo $post->getDate(); ?></p>
         <p><?php echo $post->getContent(); ?></p>
+        <span class="blog__icon blog__icon-edit"><a href="/edit?post_id=<?php echo $post->getId(); ?>"><i class="fa fa-pencil" aria-hidden="true"></i></a></span>
+        <span class="blog__ icon blog__icon-fav"><a href="#"><i class="fa fa-heart" aria-hidden="true"></i></a></span>        
     </div>
 </article>
