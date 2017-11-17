730d7dcb5e358be27874765340be3262e48b19c6
Joakim Unge <ashba@unge.se>
2017-11-17 16:18:12 +0100

    Restructure post_card.php for readability

--- src/Templates/post_card.php
+++ src/Templates/post_card.php
@@ -1,12 +1,23 @@
 <article class="col col-4 blog__post animate__fadein">
     <div class="blog__post-image">
-        <a href="<?php echo $post->getURL(); ?>"><img src="<?php echo $post->getImagePath(); ?>" alt="blog-post-image" width=100%></a>
+        <a href="<?php echo $post->getURL(); ?>">
+            <img src="<?php echo $post->getImagePath(); ?>" alt="blog-post-image" width=100%>
+        </a>
     </div>
     <div class="blog__post-content">
-        <h4 class="blog__post-title"><?php echo $post->getTitle(); ?></h4>
-        <h5 class="blog__post-author">by <?php echo $post->getAuthor(); ?></h5>
-        <p class="blog__post-timestamp"><?php echo $post->getDate(); ?></p>
-        <p><?php echo $post->getContent(); ?></p>
+        <h4 class="blog__post-title">
+            <?php echo $post->getTitle(); ?>
+        </h4>
+        <h5 class="blog__post-author">
+            by <?php echo $post->getAuthor(); ?>
+        </h5>
+        <p class="blog__post-timestamp">
+            <?php echo $post->getDate(); ?>
+        </p>
+        <p>
+            <?php echo $post->getContent(); ?>
+        </p>
+
         <?php if(isset($_SESSION['id']) && $_SESSION['admin'] === '1'): ?>
             <span class="blog__icon blog__icon-edit">
                 <a href="/edit?post_id=<?php echo $post->getId(); ?>">
@@ -14,6 +25,7 @@
                 </a>
             </span>
         <?php endif; ?>
+
         <?php if (isset($_SESSION['id'])) : ?>
             <span class="blog__icon blog__icon-fav <?php echo $post->getFavStatus(); ?>" onclick="toggleFav(<?php echo $post->getId(); ?>)" data-id="c<?php echo $post->getId(); ?>">
                 <i class="fa fa-star" aria-hidden="true"></i>
