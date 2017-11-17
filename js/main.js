7e61c04767055635acd3820a0144c124eac63472
Joakim Unge <ashba@unge.se>
2017-11-17 15:01:00 +0100

    Add ajax call for favorite marks. Refactoring.
    Rename Filtercontroller.php to ApiController.php

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ for(let i = 0; i < tags.length; i++) {
         e.preventDefault();
         let data = toggleTag(tags[i]);
         data = 'json={"tag_id":[' + data + ']}';
-        newXhr(data);
+        newXhr(data, '/api', true);
     });
 }
 
@@ -29,7 +29,14 @@ function toggleTag(tag) {
     return ajaxData;
 }
 
-function newXhr(elem) {
+function toggleFav(data) {
+    const favBtn = document.querySelector(`[data-id='c${data}']`);
+    favBtn.classList.toggle('blog__icon-fav--active');
+    data = 'id=' + data + '"';
+    newXhr(data, '/api/fav', false);
+}
+
+function newXhr(elem, route, render) {
     let xhr;
 
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
@@ -38,20 +45,24 @@ function newXhr(elem) {
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
 
-    let data = elem;
-    xhr.open("POST", "/api", true);
+    xhr.open("POST", route, true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(data);
+    xhr.send(elem);
 
-    xhr.onreadystatechange = display_data;
-    function display_data() {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                document.querySelector('#entries').innerHTML = xhr.responseText;
-            } else {
-                alert('There was a problem with the request.');
+    if (render) {
+
+        xhr.onreadystatechange = display_data;
+        function display_data() {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    document.querySelector('#entries').innerHTML = xhr.responseText;
+                } else {
+                    alert('There was a problem with the request.');
+                }
             }
         }
+
     }
+    
 }
 
---
+++ src/Controllers/ApiController.php
@@ -0,0 +1,85 @@
+<?php 
+
+namespace Blog\Controllers;
+use Blog\Controllers\AbstractController;
+use Blog\Core\Database;
+use Blog\Models\Blogpost;
+
+class ApiController extends AbstractController {
+
+    public $posts = [];
+    public $entries = [];
+
+    public function filterByTag() {
+
+        $ajaxResponse = json_decode($_POST['json'], true);
+        $db = new Database();
+        
+        $tag = $ajaxResponse['tag_id'];
+
+        if (count($tag) > 0) {
+            
+             // Build SQL 'IN' Values based on length of $tag array
+            $IN_STRING = '(';
+            foreach($tag as $value) {
+                $IN_STRING .= '?,';
+            }
+            $IN_STRING = substr($IN_STRING, 0, -1) . ')';
+    
+            $this->entries = $db->query('SELECT * FROM entry_tag JOIN entries ON entry_tag.entry_id = entries.id WHERE tag_id IN ' . $IN_STRING, $tag);
+
+            if (count($this->entries) === 0) {
+                $this->noPostsFound();
+                return;
+            }
+
+            foreach ($this->entries as $entry) {
+                $post = new Blogpost($entry['title'], $entry['content'], $entry['author'], $entry['date'], $entry['image'], $entry['id']);
+                $this->posts[] = $post;
+            }
+        
+            foreach ($this->posts as $post) {
+                include ('./src/Templates/post_card.php');
+            }
+            return;
+        }
+
+        $this->entries = $db->query('SELECT * FROM entries');
+        $this->renderBlogposts();
+        
+    }
+
+    public function renderBlogposts() {
+        foreach ($this->entries as $entry) {
+            $post = new Blogpost($entry['title'], $entry['content'], $entry['author'], $entry['date'], $entry['image'], $entry['id']);
+            $this->posts[] = $post;
+        }
+    
+        foreach ($this->posts as $post) {
+            include ('./src/Templates/post_card.php');
+        }
+    }
+
+    public function noPostsFound() {
+        include './src/Templates/postsnotfound.php';
+    }
+
+    public function markFavorite() {
+        $db = new Database();
+        $postId = $_POST['id'];
+        // $db->createOne('INSERT INTO user_fav SET entry_id = ?, user_id = ?', [$postId, $_SESSION['id']]);
+
+        $rowExists = $db->query('SELECT * FROM user_fav WHERE entry_id = ? AND user_id = ?', [$postId, $_SESSION['id']]);
+
+        if (count($rowExists) > 0) {
+            $db->query('DELETE FROM user_fav WHERE entry_id = ? AND user_id = ?', [$postId, $_SESSION['id']]);
+            return;
+        }
+
+        $db->query('INSERT INTO user_fav SET entry_id = ?, user_id = ?', [$postId, $_SESSION['id']]);
+
+    }
+
+}
+
+?>
